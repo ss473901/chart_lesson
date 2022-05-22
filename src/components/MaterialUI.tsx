10a21753fba1cfc15916b0a6bc8fc32ec405f280
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-05-22 23:25:39 +0900

    Add mui/typography

--- src/components/MaterialUI.tsx
+++ src/components/MaterialUI.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Button, makeStyles } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   btnStyle: {
     background: "green",
     padding: "3px 50px",
+  },
+  typoStyle: {
+    color: "blue",
   },
 });
 
@@ -13,6 +16,16 @@ const MaterialUI: React.FC = () => {
 
   return (
     <div>
+      <Typography
+        className={classes.typoStyle}
+        color="secondary"
+        variant="h1"
+        align="left"
+        gutterBottom
+        noWrap
+      >
+        Hello UIHello UIHello UIHello UIHello UIHello UI
+      </Typography>
       <Button
         className={classes.btnStyle}
         variant="contained"
